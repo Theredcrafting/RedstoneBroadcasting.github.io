0d9ce518e324cbe07f19a25972983169cf44f444
Redstone Broadcast Union <RedstoneBroadcasting@users.noreply.github.com>
2019-01-26 11:48:39 +0100

    mj

---
+++ rbu/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>RBU - Home </title>
+		<script
+			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="index.html"><img src="immages/rbu_logo_modern_wasser.png"></a>
+				</div>
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="index.html">Home</a></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="sites/members/index.php">Members
+						<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="sites/members/the_red_tec.php">The Red Tec</a></li>
+							<li><a href="sites/members/redstoneologe.php">Redstoneologe</a></li>
+							<li><a href="sites/members/theredcrafting.php">Theredcrafting</a></li>
+							<li><a href="sites/members/the_red_crafting.php">TheRedCafting</a></li>
+							<li><a href="sites/members/the_red_story.php">The Red Story</a></li>
+							<li><a href="sites/members/the_minecrafter_zockt.php">The Minecrafter Zockt</a></li>
+							<li><a href="sites/members/kartoffelking.php">Kartoffelking</a></li>
+							<li><a href="sites/members/thetocraft.php">Thetocraft</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="sites/projects/index.php">Projects
+							<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">The Red Tec</a></li>
+									<li><a href="#">Redstoneologe</a></li>
+									<li><a href="#">Theredcrafting</a></li>
+									<li><a href="#">TheRedCafting</a></li>
+									<li><a href="#">The Red Story</a></li>
+									<li><a href="#">The Minecrafter Zockt</a></li>
+									<li><a href="#">Kartoffelking</a></li>
+									<li><a href="#">Thetocraft</a></li>
+								</ul>
+					</li>
+					<li><a href="#">Shedule</a></li>
+					<li><a href="#">Download</a></li>
+					<li><a href="#">Log In</a></li>
+				</ul>
+			</div>
+		</nav> 	
+		<main>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>
+
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner">
+					<div class="item active">
+						<img src="la.jpg" alt="Los Angeles">
+					</div>
+					<div class="item">
+						<img src="chicago.jpg" alt="Chicago">
+					</div>
+
+					<div class="item">
+						<img src="ny.jpg" alt="New York">
+					</div>
+				</div>
+
+				<!-- Left and right controls -->
+				<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#myCarousel" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</main>
+		<footer>
+			
+		</footer>
+	</body>
+</html>
+
+		
